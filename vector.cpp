33cebf5a37a9b8cd8b940064489340f6befb4d0a
Nikunj Asodariya <truelineinstitute.info@gmail.com>
2025-01-17 14:59:35 +0530

    vector_update

--- vector.cpp
+++ vector.cpp
@@ -1,13 +1,22 @@
-#include<iostream>
-#include<vector>
-using namespace std;
+// Stop DSA series here.
 
-int main()
-{
+// hair cutting 
+// finail bottel from shivdarshan 
+// lalu shopping
 
-  
+/*
 
-    cout<<"DSA Vector programm"<<endl;
 
-    return 0;
-}
+To Whom It May Concern,
+
+This is to certify that Ms. Timol Kulsum Asifiqbal was employed with Trueline Solution from 2-dec-2024 to 29-nov-2025 as a FullStack  Developer in the Web Development Section.
+
+During her tenure with us, Ms. Timol Kulsum demonstrated exceptional skills in Web application development, including All Types of development responsibilities, Web-app architecture, Backend and Database implementation, and integration of APIs. She actively contributed to projects such as CRM management.
+
+Ms. Timol Kulsum consistently showcased professionalism, teamwork, and problem-solving abilities, which greatly contributed to the success of the Wesite Development Section. Her expertise in java ,Asp.net , flutter for frontend and php,mysql for backend was instrumental in meeting project deadlines and ensuring client satisfaction.
+
+We found Ms. Timol Kulsum to be a dedicated and hardworking professional who consistently exceeded expectations. Her ability to quickly adapt to emerging technologies and collaborate effectively with cross-functional teams was particularly commendable.
+
+We wish Ms. Timol Kulsum all the very best in her future endeavors and are confident that she will bring the same level of dedication and excellence to any organization she joins.
+
+*/
